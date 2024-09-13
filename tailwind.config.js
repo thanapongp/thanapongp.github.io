87fedaac0e700f119139ce551ad2727618043205
Thanapong Prathumchat <tanapong21@gmail.com>
2024-09-13 23:22:46 +0700

    feat(content): adds basic content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['"Raleway"', ...defaultTheme.fontFamily.sans]
+      }
+    },
   },
   plugins: [],
 }
